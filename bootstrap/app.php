52653f2939a4b7e8a670ccceef916ebcff3986c5
cladico <ion@aot.ro>
2022-05-15 10:27:46 +0000

    define paths

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,29 @@
 <?php
 
-// Load our autoloader
+define('APP_START', microtime(true));
+define('RESOURCES', dirname(__FILE__, 2).'/resources');
+define('STORAGE', dirname(__FILE__, 2).'/storage');
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 require __DIR__.'/../vendor/autoload.php';
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader('resources/views/');
+$loader = new FilesystemLoader(RESOURCES.'/views/');
 
 $twig = new Environment($loader,
   [
-    'cache' => 'storage/cache/',
+    'cache' => STORAGE.'/cache/',
     'debug' => true
   ]
 );
-
-// echo __DIR__.'/../vendor/autoload.php';
